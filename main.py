c8c87bfd8cea2f08617e0fbb944d82800873f0f8
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2025-07-07 17:59:11 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# print("Hello World!")
+
+
+# name = "Zain Khan"
+# print("Hey name is " + name)
+
+
+# isZain = False
+# if isZain == True:
+#     print("Welcome Back Zain")
+# else:
+#     print("Hello stranger")
+
+
+# name = ["Zain", "Khan", "Zain Khan"]
+# print(name[0])
+
+
+cities = ["Atlanta", "Baltimore", "Chicago",
+"Denver", "Los Angeles", "Seattle"]
+print(cities[0])
+cities.append("New York")
+print(cities[6])
