6caacb0211b5bd19f284be7c5f0fbb381e21c72d
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2025-07-08 17:26:09 +0500

    Remove age classification code and retain multiplication functionality

--- Exercise.py
+++ Exercise.py
@@ -1,12 +1,18 @@
-name = input("What's your name?")
+# name = input("What's your name?")
 
-age = int(input("Hello " + name + ".Enter your age: "))
+# age = int(input("Hello " + name + ".Enter your age: "))
 
-if age < 13:
-    print("You're an child.")
-elif age >= 13 and age < 17:
-    print("You're a teenager .")
-elif age >= 18 and age < 64:
-    print("You're an adult.")
-else:  
-    print("You're a senior citizen.")
+# if age < 13:
+#     print("You're an child.")
+# elif age >= 13 and age < 17:
+#     print("You're a teenager .")
+# elif age >= 18 and age < 64:
+#     print("You're an adult.")
+# else:  
+#     print("You're a senior citizen.")
+
+
+num = int(input("Enter a number: "))
+
+for i in range(1, 11):
+    print(f"{num} X {i} = {num * i}")
